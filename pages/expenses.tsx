db99e6dff8019fa3eefdfc09fd5e39bb85136d92
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-05 11:25:56 +0100

    :sparkles: Create three basic pages

---
+++ pages/expenses.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+const expensesPage: NextPage = () => {
+  return (
+    <div>
+      <h1>This is the Expenses page</h1>
+    </div>
+  );
+};
+
+export default expensesPage;
